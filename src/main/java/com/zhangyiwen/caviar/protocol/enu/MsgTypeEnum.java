9e227b57ab1ab64590dfd48f9b04ab9774d1ece8
zhangyiwen <zhangyiwen@youzan.com>
2017-12-26 16:54:52 +0800

    commit

--- src/main/java/com/zhangyiwen/caviar/protocol/enu/MsgTypeEnum.java
+++ src/main/java/com/zhangyiwen/caviar/protocol/enu/MsgTypeEnum.java
@@ -11,14 +11,11 @@ public enum MsgTypeEnum {
     CLIENT_LOGOUT_REQ(1002),                //客户端登出
     CLIENT_MSG_SEND_REQ(1003),              //客户端消息发送——同步
     CLIENT_MSG_SEND_ASYNC_REQ(1004),        //客户端消息发送——异步回调
-    SERVER_MSG_PUSH_REQ(1005),              //服务端消息推送
 
     //应答消息
     CLIENT_LOGIN_RESP(2001),                //客户端登录
     CLIENT_LOGOUT_RESP(2002),               //客户端登出
     CLIENT_MSG_SEND_RESP(2003),             //客户端消息发送——同步
-    CLIENT_MSG_SEND_ASYNC_RESP(2004),       //客户端消息发送——异步回调
-    SERVER_MSG_PUSH_RESP(2005),             //服务端消息推送
 
     //心跳消息
     PING(9001),                             //客户端Ping
